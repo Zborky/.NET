139b09bf4aba0005ce91f0aac8448366be533163
Zborky <jakubzboron11@gmail.com>
2024-10-25 17:04:53 +0200

    Add files via upload

---
+++ Login-System/LoginSystem/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using LoginSystem.Models;
+
+namespace LoginSystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        
+        
+    }
+}
