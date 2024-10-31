fcf73035b4d7d0dafcdace2552ddec89067ea40c
Zborky <jakubzboron11@gmail.com>
2024-10-31 11:51:22 +0100

    Add files via upload

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Sort(int[] arr) // Zmenené z BubbleSort na Sort
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Swap arr[j] a arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void Main()
+    {
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+        Sort(arr); // Zmenené z BubbleSort na Sort
+        Console.WriteLine("Zaradené pole: " + string.Join(", ", arr));
+    }
+}
